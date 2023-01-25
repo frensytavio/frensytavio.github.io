81af167b46db59dab9c0c0ad337d6abcf466d2f0
Andrea Acampora <andrea.acampora@studio.unibo.it>
2023-01-25 21:21:50 +0100

    chore: modify js form

--- js/form.js
+++ js/form.js
@@ -4,31 +4,42 @@ $(document).ready(function () {
     console.log('checking form')
     let isFormOk = true
     const input_nome = $('input#name')
-    const input_cognome = $('input#surname')
 
-    input_nome.next().remove()
     if (!input_nome.val().length > 0) {
-      nome.parent().append('<p> Il nome non può essere vuoto</p>')
       isFormOk = false
     }
 
-    input_cognome.next().remove()
-    if (!input_cognome.val().length > 0) {
-      nome.parent().append('<p> Il cognome non può essere vuoto</p>')
-      isFormOk = false
-    }
     if (isFormOk) {
-      console.log('form ok');
       $.post(
-        'https://script.google.com/macros/s/AKfycbw6XCcg1pdqQ1bZ8De2ScsCdxdlpDaOEGpfR4nZyk26GT7Dv358aqCcjfHax5UbaMqmzA/exec',
+        'https://script.google.com/macros/s/AKfycbxqxktMeWNXGMUONIyEq2r2ZDtlPPV8DxLNwlxMC8WdqMvwChTNaCDK3cEk2XNls7Lyuw/exec',
         {
-          Nome: input_nome.val(),
-          Cognome: input_cognome.val()
+          nome: input_nome.val()
         },
         function (response) {
-          console.log(response)
+          if (response == 'OK') {
+            $('h2#form-title').text('Grazie!')
+            $('form').hide()
+          } else {
+            console.log('Errore!')
+          }
         }
       )
-    }
+/*     $.ajax({
+      url: 'https://script.google.com/macros/s/AKfycbw6XCcg1pdqQ1bZ8De2ScsCdxdlpDaOEGpfR4nZyk26GT7Dv358aqCcjfHax5UbaMqmzA/exec',
+      data: { nome: input_nome.val() },
+      type: 'POST',
+      crossDomain: true,
+      dataType: 'jsonp',
+      success: function(response) {
+        if (response == 'OK') {
+          $('h2#form-title').text('Grazie!')
+          $('form').hide()
+        } else {
+          console.log('Errore!')
+        }
+      }
+    }); */
+  }
   })
+
 })
